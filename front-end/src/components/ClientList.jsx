5c3b623ced8f42831163d43c7711a7859ab4c7f4
Marco <marcoabustam@gmail.com>
2024-09-02 22:20:49 -0400

    full stack base terminada.

--- front-end/src/components/ClientList.jsx
+++ front-end/src/components/ClientList.jsx
@@ -1,17 +1,28 @@
 import {getAllClients} from '../api/clients.api.js'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
 
 
 
 export function ClientList() {
+
+    const [clients, setClients] = useState([]);
+
     useEffect(() => {
         async function fetchClients() {
             const response = await getAllClients();
-            console.log(response);
+            setClients(response.data);
         }
         fetchClients();
         }, []);
   return (
-    <div>ClienttttList</div>
+    <div>
+        <h1>Clients</h1>
+        <ul>
+            {clients.map(client => (
+                <li key={client.id}>{client.name}</li>
+            ))}
+        </ul>
+    </div>
   )
 }
